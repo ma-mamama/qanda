cdf28933ef69848427897a020758caefaf8bef3d
Ubuntu <ubuntu@ip-172-31-9-115.ap-northeast-1.compute.internal>
2022-11-28 13:39:47 +0000

    mysqlからsqliteに変更

--- rails_project/db/schema.rb
+++ rails_project/db/schema.rb
@@ -11,17 +11,17 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_31_150657) do
-  create_table "answers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "answers", force: :cascade do |t|
     t.string "name"
     t.text "content"
-    t.bigint "question_id", null: false
+    t.integer "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "good", default: 0, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
-  create_table "questions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "questions", force: :cascade do |t|
     t.string "name"
     t.string "title"
     t.text "content"
