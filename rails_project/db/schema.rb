6cd901d80f8c1462f2726a926aceff12d0f0ec83
ma-mamama <mamamamam.2525@gmail.com>
2023-03-07 23:21:27 +0900

    bestanswer_idカラムを追加するマイグレーションファイル作成

--- rails_project/db/schema.rb
+++ rails_project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_31_150657) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_05_061103) do
   create_table "answers", force: :cascade do |t|
     t.string "name"
     t.text "content"
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_31_150657) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "bestanswer_id"
   end
 
   add_foreign_key "answers", "questions"
